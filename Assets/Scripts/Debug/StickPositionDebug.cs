4e51933e524ec31b50cc82f56778e00a2a32a962
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2024-04-13 18:20:27 +0200

    Add rotator logic

---
+++ Assets/Scripts/Debug/StickPositionDebug.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StickPositionDebug : MonoBehaviour
+{
+    public string actionType;
+    public GameObject displayReference;
+    private RectTransform _rect;
+    private float _displayRate;
+    private InputAction _inputAction;
+
+    void Start()
+    {
+        _rect = GetComponent<RectTransform>();
+        _inputAction = InputSystem.actions.FindAction(actionType);
+        if (!displayReference)
+        {
+            displayReference = transform.parent.gameObject;
+        }
+        var displayReferenceRect = displayReference.GetComponent<RectTransform>().rect;
+        var shortestReferenceSide = Math.Min(displayReferenceRect.width, displayReferenceRect.height);
+        var longestOwnSide = Math.Max(_rect.rect.width, _rect.rect.height);
+        _displayRate = shortestReferenceSide / 2 - longestOwnSide;
+    }
+
+    void Update()
+    {
+        var currentInput = _inputAction.ReadValue<Vector2>();
+        var newPosition = new Vector2(currentInput.x, currentInput.y) * _displayRate;
+        _rect.anchoredPosition = newPosition;
+    }
+}
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
@@ -6,17 +7,25 @@ public class Rotator : MonoBehaviour
     public int deceleration;
     private float _speedRotate;
 
+    private Stopwatch _stopwatch = new Stopwatch();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
         GetComponent<Rigidbody2D>().AddTorque(speedRotateStart);
         // _speedRotate = speedRotateStart;
+        _stopwatch.Restart();
     }
 
     // Update is called once per frame
-    // void Update()
-    // {
-    //     transform.Rotate(Vector3.back * _speedRotate * Time.deltaTime);
-    //     _speedRotate -= deceleration * Time.deltaTime;
-    // }
+    void Update()
+    {
+        // transform.Rotate(Vector3.back * _speedRotate * Time.deltaTime);
+        // _speedRotate -= deceleration * Time.deltaTime;
+        if (GetComponent<Rigidbody2D>().angularVelocity <= 1 && _stopwatch.IsRunning && _stopwatch.ElapsedMilliseconds > 1000)
+        {
+            UnityEngine.Debug.Log($"Wheel stopped after: {_stopwatch.ElapsedMilliseconds}ms");
+            _stopwatch.Stop();
+        }
+    }
 }
